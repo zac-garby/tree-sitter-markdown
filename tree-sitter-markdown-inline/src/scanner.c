aba82cd45146ab891719f84669bb64e59442300c
Zac Garby <me@zacgarby.co.uk>
2026-03-19 15:01:22 +0000

    Rename external scanner symbols to noteboks_md_inline

--- tree-sitter-markdown-inline/src/scanner.c
+++ tree-sitter-markdown-inline/src/scanner.c
@@ -366,32 +366,32 @@ static bool scan(Scanner *s, TSLexer *lexer, const bool *valid_symbols) {
     return false;
 }
 
-void *tree_sitter_markdown_inline_external_scanner_create() {
+void *tree_sitter_noteboks_md_inline_external_scanner_create() {
     Scanner *s = (Scanner *)malloc(sizeof(Scanner));
     deserialize(s, NULL, 0);
     return s;
 }
 
-bool tree_sitter_markdown_inline_external_scanner_scan(
+bool tree_sitter_noteboks_md_inline_external_scanner_scan(
     void *payload, TSLexer *lexer, const bool *valid_symbols) {
     Scanner *scanner = (Scanner *)payload;
     return scan(scanner, lexer, valid_symbols);
 }
 
-unsigned tree_sitter_markdown_inline_external_scanner_serialize(void *payload,
+unsigned tree_sitter_noteboks_md_inline_external_scanner_serialize(void *payload,
                                                                 char *buffer) {
     Scanner *scanner = (Scanner *)payload;
     return serialize(scanner, buffer);
 }
 
-void tree_sitter_markdown_inline_external_scanner_deserialize(void *payload,
+void tree_sitter_noteboks_md_inline_external_scanner_deserialize(void *payload,
                                                               const char *buffer,
                                                               unsigned length) {
     Scanner *scanner = (Scanner *)payload;
     deserialize(scanner, buffer, length);
 }
 
-void tree_sitter_markdown_inline_external_scanner_destroy(void *payload) {
+void tree_sitter_noteboks_md_inline_external_scanner_destroy(void *payload) {
     Scanner *scanner = (Scanner *)payload;
     free(scanner);
 }
